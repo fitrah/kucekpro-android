c090d8177e75a294037a6b8a6e65317b8e4c2252
Siput Racing Touring <admin@sracing.proyek.org>
2026-08-29 22:58:00 +0800

    Handle Android back navigation

--- android/app/src/main/java/id/co/proyek/kucekpro/MainActivity.java
+++ android/app/src/main/java/id/co/proyek/kucekpro/MainActivity.java
@@ -1,6 +1,8 @@
 package id.co.proyek.kucekpro;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.activity.OnBackPressedCallback;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -8,5 +10,22 @@ public class MainActivity extends BridgeActivity {
   protected void onCreate(Bundle savedInstanceState) {
     registerPlugin(ThermalPrinterPlugin.class);
     super.onCreate(savedInstanceState);
+    configureBackButton();
+  }
+
+  private void configureBackButton() {
+    getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+      @Override
+      public void handleOnBackPressed() {
+        WebView webView = bridge != null ? bridge.getWebView() : null;
+
+        if (webView != null && webView.canGoBack()) {
+          webView.goBack();
+          return;
+        }
+
+        moveTaskToBack(true);
+      }
+    });
   }
 }
